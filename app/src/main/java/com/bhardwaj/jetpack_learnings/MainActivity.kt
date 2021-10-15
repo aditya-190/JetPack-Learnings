436da649c90e571d2811b53d4ab974bec04de1d8
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-15 18:41:48 +0530

    Learn to make composable and preview function.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Aditya Bhardwaj")
+            MessageCard(name = "Aditya Bhardwaj")
         }
     }
+}
+
+@Preview
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
 }
