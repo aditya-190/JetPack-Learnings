3146b2d3c397aabfd26774b0f563bd21d4d5c45c
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-15 18:36:41 +0530

    Changes Done.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -2,9 +2,15 @@ package com.bhardwaj.jetpack_learnings
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Text("Aditya Bhardwaj")
+        }
     }
 }
