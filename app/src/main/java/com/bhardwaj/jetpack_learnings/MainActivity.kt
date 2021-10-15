e7289b20e554db8d8c0411b0d88b5b3afe6d6c7f
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-15 18:32:30 +0530

    Detailed Commit (Auto Tracker) - Modified: .gradle/7.0.2/fileHashes/fileHashes.lock - From Auto Tracker. Modified: app/build.gradle - From Auto Tracker. Modified: app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt - From Auto Tracker. Modified: app/src/main/res/values-night/themes.xml - From Auto Tracker. Modified: app/src/main/res/values/themes.xml - From Auto Tracker. Modified: build.gradle - From Auto Tracker. Modified: settings.gradle - From Auto Tracker. Removed: .idea/.gitignore - From Auto Tracker. Removed: .idea/JetPack-Learnings.iml - From Auto Tracker. Removed: .idea/compiler.xml - From Auto Tracker. Removed: .idea/gradle.xml - From Auto Tracker. Removed: .idea/misc.xml - From Auto Tracker. Removed: .idea/modules.xml - From Auto Tracker. Removed: .idea/vcs.xml - From Auto Tracker.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -2,37 +2,9 @@ package com.bhardwaj.jetpack_learnings
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.bhardwaj.jetpack_learnings.ui.theme.JetPackLearningsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetPackLearningsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Aditya")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetPackLearningsTheme {
-        Greeting("Android")
     }
 }
