b41ebc902fd390a57aa3175bcd1cde214183e34a
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-16 21:42:23 +0530

    Detailed Commit (Auto Tracker) - Modified: .gradle/7.0.2/executionHistory/executionHistory.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.bin - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.lock - From Auto Tracker. Modified: .gradle/buildOutputCleanup/buildOutputCleanup.lock - From Auto Tracker. Modified: app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt - From Auto Tracker.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -19,11 +20,84 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+data class Message(
+    val author: String,
+    val body: String
+)
+
+val conversationSample = listOf(
+    Message(
+        "Colleague",
+        "Test...Test...Test..."
+    ),
+    Message(
+        "Colleague",
+        "List of Android versions:\n" +
+                "Android KitKat (API 19)\n" +
+                "Android Lollipop (API 21)\n" +
+                "Android Marshmallow (API 23)\n" +
+                "Android Nougat (API 24)\n" +
+                "Android Oreo (API 26)\n" +
+                "Android Pie (API 28)\n" +
+                "Android 10 (API 29)\n" +
+                "Android 11 (API 30)\n" +
+                "Android 12 (API 31)\n"
+    ),
+    Message(
+        "Colleague",
+        "I think Kotlin is my favorite programming language.\n" +
+                "It's so much fun!"
+    ),
+    Message(
+        "Colleague",
+        "Searching for alternatives to XML layouts..."
+    ),
+    Message(
+        "Colleague",
+        "Hey, take a look at Jetpack Compose, it's great!\n" +
+                "It's the Android's modern toolkit for building native UI." +
+                "It simplifies and accelerates UI development on Android." +
+                "Less code, powerful tools, and intuitive Kotlin APIs :)"
+    ),
+    Message(
+        "Colleague",
+        "It's available from API 21+ :)"
+    ),
+    Message(
+        "Colleague",
+        "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+    ),
+    Message(
+        "Colleague",
+        "Android Studio next version's name is Arctic Fox"
+    ),
+    Message(
+        "Colleague",
+        "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+    ),
+    Message(
+        "Colleague",
+        "I didn't know you can now run the emulator directly from Android Studio"
+    ),
+    Message(
+        "Colleague",
+        "Compose Previews are great to check quickly how a composable layout looks like"
+    ),
+    Message(
+        "Colleague",
+        "Previews are also interactive after enabling the experimental setting"
+    ),
+    Message(
+        "Colleague",
+        "Have you tried writing build.gradle with KTS?"
+    ),
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Colleague", "Hey, take a look Jetpack Compose. It's great!"))
+            Conversation(messages = conversationSample)
         }
     }
 }
@@ -31,11 +105,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Conversation(messages: List<Message>) {
     LazyColumn{
-        items(messages) {
-            
+        items(messages) { message ->
+            MessageCard(msg = message)
         }
     }
-
 }
 
 @Composable
@@ -72,22 +145,8 @@ fun MessageCard(msg: Message) {
     }
 }
 
-data class Message(
-    val author: String,
-    val body: String
-)
-
 @Preview(
     name = "Light Mode",
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     showBackground = true
 )
-@Preview(
-    name = "Dark Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true
-)
-@Composable
-fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleague", "Hey, take a look Jetpack Compose. It's great!"))
-}
