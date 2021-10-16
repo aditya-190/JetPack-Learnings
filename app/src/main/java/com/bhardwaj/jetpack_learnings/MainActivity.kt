8ddace8f68d467174e478ae1b093e1635b843338
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-16 21:11:58 +0530

    Detailed Commit (Auto Tracker) - Modified: .gradle/7.0.2/executionHistory/executionHistory.bin - From Auto Tracker. Modified: .gradle/7.0.2/executionHistory/executionHistory.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.bin - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/resourceHashesCache.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/classAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/jarAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/javaCompile.lock - From Auto Tracker. Modified: .gradle/buildOutputCleanup/buildOutputCleanup.lock - From Auto Tracker. Modified: app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt - From Auto Tracker. Added: .idea/assetWizardSettings.xml - From Auto Tracker. Added: app/src/main/res/drawable/logo.jpeg - From Auto Tracker.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
+import java.nio.file.Files.size
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +31,11 @@ class MainActivity : ComponentActivity() {
 fun MessageCard(msg: Message) {
     Row {
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Sample Image"
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Sample Image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
         Column {
             Text(text = msg.author)
