df0ffb1c750a73c3196034e96a011c466693d6f3
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-16 21:52:34 +0530

    Detailed Commit (Auto Tracker) - Modified: .gradle/7.0.2/executionHistory/executionHistory.bin - From Auto Tracker. Modified: .gradle/7.0.2/executionHistory/executionHistory.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.bin - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/resourceHashesCache.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/classAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/jarAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/javaCompile.lock - From Auto Tracker. Modified: .gradle/buildOutputCleanup/buildOutputCleanup.lock - From Auto Tracker. Modified: app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt - From Auto Tracker.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -3,6 +3,8 @@ package com.bhardwaj.jetpack_learnings
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -16,6 +18,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -125,6 +128,9 @@ fun MessageCard(msg: Message) {
         Spacer(modifier = Modifier.width(8.dp))
 
         var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
 
         Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
@@ -135,7 +141,14 @@ fun MessageCard(msg: Message) {
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(all = 1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
