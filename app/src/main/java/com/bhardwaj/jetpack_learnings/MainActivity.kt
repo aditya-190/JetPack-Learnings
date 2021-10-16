84eb0cf7bfb9f57ef39e4cb84beb7b6a7ad4a93c
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-16 21:22:07 +0530

    Detailed Commit (Auto Tracker) - Modified: .gradle/7.0.2/executionHistory/executionHistory.bin - From Auto Tracker. Modified: .gradle/7.0.2/executionHistory/executionHistory.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.bin - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/resourceHashesCache.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/classAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/jarAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/javaCompile.lock - From Auto Tracker. Modified: .gradle/buildOutputCleanup/buildOutputCleanup.lock - From Auto Tracker. Modified: app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt - From Auto Tracker.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,7 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Aditya", "Learning Jetpack Compose"))
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
@@ -32,13 +34,22 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
+
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Text(
+                text = msg.body,
+                style = MaterialTheme.typography.body2
+            )
         }
     }
 }
