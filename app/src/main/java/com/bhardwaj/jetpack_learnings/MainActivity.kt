3dd77d370fd4c08837be5634dba53dfdd915447a
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-19 10:33:15 +0530

    Detailed Commit (Auto Tracker) - Modified: .gradle/7.0.2/executionHistory/executionHistory.bin - From Auto Tracker. Modified: .gradle/7.0.2/executionHistory/executionHistory.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.bin - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/fileHashes.lock - From Auto Tracker. Modified: .gradle/7.0.2/fileHashes/resourceHashesCache.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/classAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/jarAnalysis.bin - From Auto Tracker. Modified: .gradle/7.0.2/javaCompile/javaCompile.lock - From Auto Tracker. Modified: .gradle/buildOutputCleanup/buildOutputCleanup.lock - From Auto Tracker. Modified: app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt - From Auto Tracker.

--- app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/jetpack_learnings/MainActivity.kt
@@ -12,9 +12,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,140 +20,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
-data class Message(
-    val author: String,
-    val body: String
-)
-
-val conversationSample = listOf(
-    Message(
-        "Colleague",
-        "Test...Test...Test..."
-    ),
-    Message(
-        "Colleague",
-
-        "List of Android versions:\n" +
-                "Android KitKat (API 19)\n" +
-                "Android Lollipop (API 21)\n" +
-                "Android Marshmallow (API 23)\n" +
-                "Android Nougat (API 24)\n" +
-                "Android Oreo (API 26)\n" +
-                "Android Pie (API 28)\n" +
-                "Android 10 (API 29)\n" +
-                "Android 11 (API 30)\n" +
-                "Android 12 (API 31)\n"
-    ),
-    Message(
-        "Colleague",
-        "I think Kotlin is my favorite programming language.\n" +
-                "It's so much fun!"
-    ),
-    Message(
-        "Colleague",
-        "Searching for alternatives to XML layouts..."
-    ),
-    Message(
-        "Colleague",
-        "Hey, take a look at Jetpack Compose, it's great!\n" +
-                "It's the Android's modern toolkit for building native UI." +
-                "It simplifies and accelerates UI development on Android." +
-                "Less code, powerful tools, and intuitive Kotlin APIs :)"
-    ),
-    Message(
-        "Colleague",
-        "It's available from API 21+ :)"
-    ),
-    Message(
-        "Colleague",
-        "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
-    ),
-    Message(
-        "Colleague",
-        "Android Studio next version's name is Arctic Fox"
-    ),
-    Message(
-        "Colleague",
-        "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
-    ),
-    Message(
-        "Colleague",
-        "I didn't know you can now run the emulator directly from Android Studio"
-    ),
-    Message(
-        "Colleague",
-        "Compose Previews are great to check quickly how a composable layout looks like"
-    ),
-    Message(
-        "Colleague",
-        "Previews are also interactive after enabling the experimental setting"
-    ),
-    Message(
-        "Colleague",
-        "Have you tried writing build.gradle with KTS?"
-    ),
-)
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Conversation(messages = conversationSample)
-        }
-    }
-}
+            Scaffold {
+                TopAppBar {
+                    
+                }
+                BottomAppBar {
+                    
+                }
+                FloatingActionButton(onClick = {}) {
 
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(msg = message)
-        }
-    }
-}
-
-@Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 16.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "Sample Image",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(4.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
-
-        Spacer(modifier = Modifier.width(16.dp))
-
-        var isExpanded by remember { mutableStateOf(false) }
-        val surfaceColor: Color by animateColorAsState(
-            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
-        )
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                elevation = 1.dp,
-                color = surfaceColor,
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(all = 1.dp)
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 8.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.body2
-                )
+                }
             }
         }
     }
