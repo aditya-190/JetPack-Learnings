5fb2c667d4e1a3e2048a92ccfdf7374510d782e3
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-23 09:41:31 +0530

    Detailed Commit (Auto Tracker) - Modified: Jetpack_Basics/.idea/misc.xml - From Auto Tracker. Modified: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt - From Auto Tracker.

--- Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt
+++ Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt
@@ -3,10 +3,18 @@ package com.bhardwaj.jetpackbasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.bhardwaj.jetpackbasics.ui.theme.JetpackBasicsTheme
 
@@ -14,6 +22,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            UseOfColumn()
         }
+    }
+}
+
+@Preview
+@Composable
+fun UseOfColumn() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(Color.LightGray)
+            .fillMaxWidth(1f)
+    ) {
+        Text(text = "Hello")
+        Text(text = "World")
+    }
+}
+
+@Composable
+fun UseOfRow() {
+    Row {
+        Text(text = "Hello")
+        Text(text = "World")
     }
 }
