a93c965082964589e0fb237cea5c3d2b4e412bd2
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-23 09:23:59 +0530

    Detailed Commit (Auto Tracker) - Added: Jetpack_Basics/.gitignore - From Auto Tracker. Added: Jetpack_Basics/.idea/.gitignore - From Auto Tracker. Added: Jetpack_Basics/.idea/.name - From Auto Tracker. Added: Jetpack_Basics/.idea/gradle.xml - From Auto Tracker. Added: Jetpack_Basics/.idea/misc.xml - From Auto Tracker. Added: Jetpack_Basics/.idea/vcs.xml - From Auto Tracker. Added: Jetpack_Basics/app/.gitignore - From Auto Tracker. Added: Jetpack_Basics/app/build.gradle - From Auto Tracker. Added: Jetpack_Basics/app/proguard-rules.pro - From Auto Tracker. Added: Jetpack_Basics/app/src/androidTest/java/com/bhardwaj/jetpackbasics/ExampleInstrumentedTest.kt - From Auto Tracker. Added: Jetpack_Basics/app/src/main/AndroidManifest.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt - From Auto Tracker. Added: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/ui/theme/Color.kt - From Auto Tracker. Added: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/ui/theme/Shape.kt - From Auto Tracker. Added: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/ui/theme/Theme.kt - From Auto Tracker. Added: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/ui/theme/Type.kt - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/drawable-v24/ic_launcher_foreground.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/drawable/ic_launcher_background.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-hdpi/ic_launcher.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-hdpi/ic_launcher_round.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-mdpi/ic_launcher.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-xhdpi/ic_launcher.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-xhdpi/ic_launcher_round.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-xxhdpi/ic_launcher.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/values-night/themes.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/values/colors.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/values/strings.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/main/res/values/themes.xml - From Auto Tracker. Added: Jetpack_Basics/app/src/test/java/com/bhardwaj/jetpackbasics/ExampleUnitTest.kt - From Auto Tracker. Added: Jetpack_Basics/build.gradle - From Auto Tracker. Added: Jetpack_Basics/gradle.properties - From Auto Tracker. Added: Jetpack_Basics/gradle/wrapper/gradle-wrapper.jar - From Auto Tracker. Added: Jetpack_Basics/gradle/wrapper/gradle-wrapper.properties - From Auto Tracker. Added: Jetpack_Basics/gradlew - From Auto Tracker. Added: Jetpack_Basics/gradlew.bat - From Auto Tracker. Added: Jetpack_Basics/settings.gradle - From Auto Tracker.

---
+++ Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.bhardwaj.jetpackbasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.bhardwaj.jetpackbasics.ui.theme.JetpackBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackBasicsTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Aditya Bhardwaj")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Android")
+}
