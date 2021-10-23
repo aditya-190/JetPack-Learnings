46f8bdb663f804d446bbd1f6df5031ebac6063f4
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-23 10:12:56 +0530

    Detailed Commit (Auto Tracker) - Modified: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt - From Auto Tracker.

--- Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt
+++ Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bhardwaj.jetpackbasics.ui.theme.JetpackBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,5 +56,27 @@ fun UseOfRow() {
         Text(text = "Hello")
         Text(text = "World")
         Text(text = "Hello")
+    }
+}
+
+@Preview
+@Composable
+fun UseOfInteractions() {
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxSize()
+            .border(1.dp, Color.Black)
+            .padding(17.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Hello",
+            modifier = Modifier
+                .clickable {
+
+                }
+        )
+        Text(text = "World")
     }
 }
