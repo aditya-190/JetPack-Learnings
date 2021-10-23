cc1a042a62a40f908ce95472eccd7c27c3a49845
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-23 10:53:49 +0530

    Detailed Commit (Auto Tracker) - Modified: Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt - From Auto Tracker.

--- Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt
+++ Jetpack_Basics/app/src/main/java/com/bhardwaj/jetpackbasics/MainActivity.kt
@@ -16,24 +16,34 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bhardwaj.jetpackbasics.ui.theme.JetpackBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MakingMaterialCardView(
-                painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = "Simply an Image.",
-                title = "Aditya Bhardwaj Card View"
-            )
+            Column() {
+                MakingMaterialCardView(
+                    painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Simply an Image.",
+                    title = "Aditya Bhardwaj Card View"
+                )
+                MakingMaterialCardView(
+                    painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Simply an Image.",
+                    title = "Aditya Bhardwaj Card View"
+                )
+            }
         }
     }
 }
@@ -98,8 +108,8 @@ fun MakingMaterialCardView(
     Card(
         modifier = Modifier
             .padding(16.dp)
-            .fillMaxWidth(0.7f)
-            .fillMaxHeight(0.5f),
+            .fillMaxWidth(0.5f)
+            .fillMaxHeight(0.4f),
         shape = RoundedCornerShape(16.dp),
         elevation = 6.dp
     ) {
@@ -111,12 +121,29 @@ fun MakingMaterialCardView(
                 modifier = Modifier.fillMaxSize()
             )
 
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            )
+                        )
+                    )
+            )
+
             Text(
                 text = title,
                 modifier = Modifier
                     .fillMaxWidth()
                     .align(Alignment.BottomStart)
-                    .padding(12.dp)
+                    .padding(12.dp),
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
             )
         }
     }
