10ecca77199616bc5d97b1c1c3d21735e01be4b1
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-27 21:06:09 +0530

    Detailed Commit (Auto Tracker) - Modified: MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/MainActivity.kt - From Auto Tracker. Modified: MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/ui/HomeScreen.kt - From Auto Tracker.

--- MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/MainActivity.kt
+++ MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/MainActivity.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import com.bhardwaj.meditationapp.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MeditationUI()
+            HomeScreen()
         }
     }
-}
-
-@Composable
-fun MeditationUI() {
-
 }
