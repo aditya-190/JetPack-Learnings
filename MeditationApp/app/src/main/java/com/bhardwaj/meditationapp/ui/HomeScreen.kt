acc5a3a1f1a5b4f2428fdeed84b0eb1ec008a3d8
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-27 21:27:35 +0530

    Detailed Commit (Auto Tracker) - Modified: MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/ui/HomeScreen.kt - From Auto Tracker.

--- MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/ui/HomeScreen.kt
+++ MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/ui/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -134,6 +135,7 @@ fun CurrentMeditation(
                 tint = Color.White,
                 modifier = Modifier
                     .background(ButtonBlue)
+                    .clip(CircleShape)
                     .padding(8.dp)
                     .size(24.dp)
             )
