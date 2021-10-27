b16fb574c4c134a9c06b2cc544c7c55c3466df75
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-27 21:13:15 +0530

    Detailed Commit (Auto Tracker) - Modified: MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/ui/HomeScreen.kt - From Auto Tracker.

--- MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/ui/HomeScreen.kt
+++ MeditationApp/app/src/main/java/com/bhardwaj/meditationapp/ui/HomeScreen.kt
@@ -1,18 +1,25 @@
 package com.bhardwaj.meditationapp.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.bhardwaj.meditationapp.R
+import com.bhardwaj.meditationapp.ui.theme.ButtonBlue
+import com.bhardwaj.meditationapp.ui.theme.DarkerButtonBlue
 import com.bhardwaj.meditationapp.ui.theme.DeepBlue
+import com.bhardwaj.meditationapp.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen() {
@@ -65,6 +72,28 @@ fun HeadingSection(name: String = "Aditya") {
 }
 
 @Composable
-fun SubHeadingChips() {
-
+fun SubHeadingChips(
+    chips: List<String> = listOf("Sweet", "Sleep", "Depression", "Insomia")
+) {
+    var selectedChipIndex by remember { mutableStateOf(0) }
+    LazyRow {
+        items(chips.size) {
+            Box(
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 16.dp, bottom = 16.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(if (selectedChipIndex == it) ButtonBlue else DarkerButtonBlue)
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = chips[it],
+                    color = TextWhite
+                )
+            }
+        }
+    }
 }
