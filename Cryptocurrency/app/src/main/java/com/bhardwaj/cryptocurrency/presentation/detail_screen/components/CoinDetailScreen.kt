9e214c453e408f93c3152a32fd9c732631d7c093
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-31 03:00:39 +0530

    Detailed Commit (Auto Tracker) - Modified: Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/detail_screen/components/CoinDetailScreen.kt - From Auto Tracker.

--- Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/detail_screen/components/CoinDetailScreen.kt
+++ Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/detail_screen/components/CoinDetailScreen.kt
@@ -1,10 +1,9 @@
 package com.bhardwaj.cryptocurrency.presentation.detail_screen.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,35 +14,84 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.bhardwaj.cryptocurrency.domain.model.Coin
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.bhardwaj.cryptocurrency.presentation.Screen
+import com.bhardwaj.cryptocurrency.presentation.detail_screen.CoinDetailViewModel
+import com.bhardwaj.cryptocurrency.presentation.list_screen.CoinListViewModel
+import com.bhardwaj.cryptocurrency.presentation.list_screen.components.CoinListItem
 
 @Composable
-fun CoinListItem(
-    coin: Coin,
-    onItemClick: (Coin) -> Unit
+fun CoinDetailScreen(
+    viewModel: CoinDetailViewModel = hiltViewModel()
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                onItemClick(coin)
-            }
-            .padding(20.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
+    val state = viewModel.state.value
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
-            style = MaterialTheme.typography.body1,
-            overflow = TextOverflow.Ellipsis
-        )
+        state.coin?.let { coin ->
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(20.dp)
+            ) {
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
+                            style = MaterialTheme.typography.h2,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.weight(8F)
+                        )
 
-        Text(
-            text = if (coin.isActive) "active" else "inactive",
-            color = if (coin.isActive) Color.Green else Color.Red,
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.End,
-            style = MaterialTheme.typography.body2,
-            modifier = Modifier.align(alignment = Alignment.CenterVertically)
-        )
+                        Text(
+                            text = if (coin.isActive) "active" else "inactive",
+                            color = if (coin.isActive) Color.Green else Color.Red,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.End,
+                            style = MaterialTheme.typography.body2,
+                            modifier = Modifier
+                                .align(alignment = Alignment.CenterVertically)
+                                .weight(2F)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    Text(
+                        text = coin.description,
+                        style = MaterialTheme.typography.body2,
+                    )
+
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    Text(
+                        text = "Tags",
+                        style = MaterialTheme.typography.h3,
+                    )
+
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+            }
+        }
+
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
     }
 }
