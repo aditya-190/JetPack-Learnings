4ad5d4c14a4a7ede508a9a8145e3da47a942effc
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-31 03:13:51 +0530

    Detailed Commit (Auto Tracker) - Modified: Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/domain/use_case/get_list/GetCoinsUseCase.kt - From Auto Tracker.

--- Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/domain/use_case/get_list/GetCoinsUseCase.kt
+++ Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/domain/use_case/get_list/GetCoinsUseCase.kt
@@ -15,7 +15,7 @@ class GetCoinsUseCase @Inject constructor(
 ) {
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading<List<Coin>>())
             val coins = repository.getCoins().map { it.toCoin() }
             emit(Resource.Success(coins))
         } catch (e: HttpException) {
