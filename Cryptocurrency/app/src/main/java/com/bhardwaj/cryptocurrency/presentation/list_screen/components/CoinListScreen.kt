218b2e1257c4618eb22ba84851dfff5bb080982a
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-31 02:03:27 +0530

    Detailed Commit (Auto Tracker) - Modified: Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/list_screen/components/CoinListItem.kt - From Auto Tracker. Modified: Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/list_screen/components/CoinListScreen.kt - From Auto Tracker.

--- Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/list_screen/components/CoinListScreen.kt
+++ Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/list_screen/components/CoinListScreen.kt
@@ -1,4 +1,13 @@
 package com.bhardwaj.cryptocurrency.presentation.list_screen.components
 
-class CoinListScreen {
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.bhardwaj.cryptocurrency.presentation.list_screen.CoinListViewModel
+
+@Composable
+fun CoinListScreen(
+    navController: NavController,
+    viewModel: CoinListViewModel = hiltViewModel()
+) {
 }
