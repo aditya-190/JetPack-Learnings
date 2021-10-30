b755b721cda7b1308cbf53abc81cd79b154833d4
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-10-31 02:51:34 +0530

    Detailed Commit (Auto Tracker) - Modified: Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/detail_screen/components/CoinTeamItem.kt - From Auto Tracker. Added: Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/detail_screen/components/CoinDetailScreen.kt - From Auto Tracker.

--- Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/detail_screen/components/CoinTeamItem.kt
+++ Cryptocurrency/app/src/main/java/com/bhardwaj/cryptocurrency/presentation/detail_screen/components/CoinTeamItem.kt
@@ -1,49 +1,32 @@
-package com.bhardwaj.cryptocurrency.presentation.list_screen.components
+package com.bhardwaj.cryptocurrency.presentation.detail_screen.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.bhardwaj.cryptocurrency.domain.model.Coin
+import com.bhardwaj.cryptocurrency.data.remote.dto.TeamMember
 
 @Composable
-fun CoinListItem(
-    coin: Coin,
-    onItemClick: (Coin) -> Unit
+fun CoinTeamItem(
+    teamMember: TeamMember,
+    modifier: Modifier = Modifier
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                onItemClick(coin)
-            }
-            .padding(20.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
-            style = MaterialTheme.typography.body1,
-            overflow = TextOverflow.Ellipsis
+            text = teamMember.name,
+            style = MaterialTheme.typography.h4,
         )
-
+        Spacer(modifier = Modifier.height(4.dp))
         Text(
-            text = if (coin.isActive) "active" else "inactive",
-            color = if (coin.isActive) Color.Green else Color.Red,
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.End,
+            text = teamMember.position,
             style = MaterialTheme.typography.body2,
-            modifier = Modifier.align(alignment = Alignment.CenterVertically)
+            fontStyle = FontStyle.Italic
         )
     }
 }
