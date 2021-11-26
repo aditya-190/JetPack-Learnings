a1143d66f6b0100516bbb5aa49c0d6593b993b9e
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-11-26 23:45:15 +0530

    Initial Project

--- 100Jetpack/app/src/main/java/com/bhardwaj/daily/MainActivity.kt
+++ 100Jetpack/app/src/main/java/com/bhardwaj/daily/MainActivity.kt
@@ -3,11 +3,23 @@ package com.bhardwaj.daily
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bhardwaj.daily.ui.theme.HundredDaysOfComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HundredDaysOfComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HundredDaysOfComposeTheme {
-        Greeting("Android")
     }
 }
